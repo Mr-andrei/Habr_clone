d57cc535aab668ed87dd3a05e8fc181f086c8192
Mr-andrei <mr.chauskek@gmail.com>
2023-11-16 22:44:57 +0400

    user login

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -9,9 +9,6 @@ import { Theme } from '../../src/app/providers/ThemeProvider';
 import {
     RouterDecorator,
 } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import {
-    TranslationDecorator,
-} from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -26,4 +23,3 @@ export const parameters = {
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
-addDecorator(TranslationDecorator());
