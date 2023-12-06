b56eb594ebc479731a45e10a2b571786f54f9bb9
Mr-andrei <mr.chauskek@gmail.com>
2023-12-06 23:02:54 +0300

    create instance

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -4,10 +4,17 @@ import { userReducer } from 'entities/User';
 import {
     createReducerManager,
 } from 'app/providers/StoreProvider/config/reducerManager';
+import { $api } from 'shared/api/api';
+import { NavigateOptions } from 'react-router';
+import { To } from 'react-router-dom';
 import { StateSchema } from './StateSchema';
 
 export function
-createReduxStore(initialState?: StateSchema, asyncReducers?: ReducersMapObject<StateSchema>) {
+createReduxStore(
+    initialState?: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+    navigate?:(to: To, options?: NavigateOptions)=> void,
+) {
     const rootReducers: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
         counter: counterReducer,
@@ -16,10 +23,18 @@ createReduxStore(initialState?: StateSchema, asyncReducers?: ReducersMapObject<S
 
     const reducerManager = createReducerManager(rootReducers);
 
-    const store = configureStore<StateSchema>({
+    const store = configureStore({
         reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            thunk: {
+                extraArgument: {
+                    api: $api,
+                    navigate,
+                },
+            },
+        }),
     });
 
     // @ts-ignore
