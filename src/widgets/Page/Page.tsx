6f06e2fbdefda2b9a9cfea8da6990eec882656bc
Mr-andrei <mr.chauskek@gmail.com>
2024-01-05 18:09:54 +0300

    Create virtuileze

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -22,6 +22,8 @@ interface PageProps {
     onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
     const {
         className,
@@ -56,6 +58,7 @@ export const Page = memo((props: PageProps) => {
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd && <div className={cls.trigger} ref={triggerRef} />}
